0fae0cafdd51e863cd07cc4b8dbf67564fed7caf
Abdulla Shoymardonov <abdulla1998uz@gmail.com>
2023-03-15 16:37:43 +0500

    Initial commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from './MealsSummary.module.css'
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious food, Delivered to you</h2>
+      <p>
+        Choose your meal Choose your mealChoose your mealChoose your mealChoose
+        your mealChoose your mealChoose your meal
+      </p>
+      <p>
+        All our meals are cooked with high quality ingredients, just-in-time and
+        of course by experienced chefs!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
